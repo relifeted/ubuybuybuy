0e847a221882efa24473c33dd7857dfa35fd1262
Ted <ted.chen@104.com.tw>
2018-06-13 20:55:42 +0800

    pivot to support some meme

--- backpack.config.js
+++ backpack.config.js
@@ -1,8 +1,14 @@
 const Dotenv = require('dotenv-webpack')
+// const WebpackBar = require('webpackbar')
 
 module.exports = {
   webpack: (config, options, webpack) => {
     config.plugins.push(new Dotenv())
+    // config.plugins.push(
+    //   new WebpackBar({
+    //     profile: true,
+    //   })
+    // )
     return config
   },
 }
--- src/middleware/replyImageLINE.js
+++ src/middleware/replyImageLINE.js
@@ -1,10 +1,19 @@
 import { Line } from 'messaging-api-line'
 
-const images = [
+const buybuybuyImages = [
   'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/1.gif',
   'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/2.jpg',
   'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/3.jpg',
   'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/4.jpg',
+]
+
+const pantherHeadImages = [
+  'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/panther_head_1.jpg',
+  'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/panther_head_2.jpg',
+  'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/panther_head_3.jpg',
+  'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/panther_head_4.jpg',
+  'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/panther_head_5.jpg',
+  'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/panther_head_6.jpg',
 ]
 
 function getRandomInt(max) {
@@ -13,12 +22,27 @@ function getRandomInt(max) {
 
 export default async function handler(context, next) {
   console.log('context.intent:', context.intent)
-  if (context.intent && context.intent === 'buy buy buy') {
-    const index = getRandomInt(images.length)
+  let image
+  if (context.intent && context.intent === 'support buybuybuy') {
+    const index = getRandomInt(buybuybuyImages.length)
     console.log('index:', index)
-    const image = images[index]
-    console.log('image:', image)
+    image = buybuybuyImages[index]
+  } else if (context.intent && context.intent === 'support panther head') {
+    const index = getRandomInt(pantherHeadImages.length)
+    console.log('index:', index)
+    image = pantherHeadImages[index]
+  }
+  console.log('image:', image)
+  if (image) {
     const message = Line.createImage(image)
+    console.log('message:', message)
+    await context.reply([message])
+  }
+  if (context.intent && context.intent === 'support lonely god') {
+    const message = Line.createVideo(
+      'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/lonely_god_1.mp4',
+      'https://s3-us-west-2.amazonaws.com/ubuybuybuy-us-west-2/lonely_god_1.jpg'
+    )
     console.log('message:', message)
     await context.reply([message])
   }
