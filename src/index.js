3b16f9d23f38757e5d0e21edd57a05e2b2809ca9
Ted <ted.chen@104.com.tw>
2018-06-17 13:36:12 +0800

    remove log

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import { createRequestHandler } from 'bottender-lambda'
 import bots from './bots'
 
 export const line = (_event, _context, callback) => {
-  console.log('apiw event:', JSON.stringify(_event))
+  // console.log('apiw event:', JSON.stringify(_event))
   const handler = createRequestHandler(bots.line)
   const { context } = _event
   const event = {
     httpMethod: context['http-method'],
     body: _event['body-json'],
   }
+  console.log('event:', JSON.stringify(event))
   handler(event, _context, callback)
 }
 
