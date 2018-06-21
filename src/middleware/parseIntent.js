41c289711a0a19915ab25562a3adbc51a10be403
Ted <ted.chen@104.com.tw>
2018-06-21 13:12:19 +0800

    reduce luis api usage

--- src/middleware/parseIntent.js
+++ src/middleware/parseIntent.js
@@ -8,15 +8,18 @@ const luis = new LUISClient({
 })
 
 export default async function handler(context, next) {
-  const response = await new Promise((resolve, reject) => {
-    luis.predict(context.event.text, {
-      onSuccess: resolve,
-      onFailure: reject,
+  const { text = '' } = context.event
+  if (text.indexOf('支援') >= 0) {
+    const response = await new Promise((resolve, reject) => {
+      luis.predict(context.event.text, {
+        onSuccess: resolve,
+        onFailure: reject,
+      })
     })
-  })
-  console.log('luis response:', response)
-  if (response.topScoringIntent.score > 0.5) {
-    context.intent = response.topScoringIntent.intent
+    console.log('luis response:', response)
+    if (response.topScoringIntent.score > 0.5) {
+      context.intent = response.topScoringIntent.intent
+    }
+    next()
   }
-  next()
 }
