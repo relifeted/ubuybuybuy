2cfe33c22f268760d2fdd1518bf7b82d6ef93f12
Ted <ted.chen@104.com.tw>
2018-08-20 21:39:59 +0800

    remove luis.ai usage

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import bots from './bots'
 export const line = (_event, _context, callback) => {
   // console.log('apiw event:', JSON.stringify(_event))
   const handler = createRequestHandler(bots.line)
-  const { context } = _event
+  // const { context } = _event
   const event = {
-    httpMethod: context['http-method'],
-    body: _event['body-json'],
+    httpMethod: _event.method,
+    body: _event.body,
   }
   console.log('event:', JSON.stringify(event))
   handler(event, _context, callback)
